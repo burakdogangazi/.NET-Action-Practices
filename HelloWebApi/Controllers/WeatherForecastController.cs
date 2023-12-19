7292c56a4cb2c533cefad2cb8cc18b47fab69cfc
Burak Doğan <burak.dogan1@huawei.com>
2023-12-19 14:48:42 +0300

    Actions added.

---
+++ HelloWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]s")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+
+
+        //api/WeatherForecases?id=3 -- From Query
+        [HttpGet("~/getById")]
+        public IEnumerable<WeatherForecast> GetById([FromQuery] string id)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+        
+        //From Query
+        [HttpGet("{id}")]
+        public IEnumerable<WeatherForecast> GetByIdSecond(string id)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+
+
+        //api/WeatherForecases/3 -- From Route
+        [HttpGet("{id}")]
+        public IEnumerable<WeatherForecast> GetForecast(string id)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+    }
+
+
+    //Actions--> From Body(Employee e), From Query(QueryParam?=), FromRoute(/product/25)
+    
+    // Yanlış - Doğru -- DELETE /Books/deleteBook/1 , DELETE Books/1, GET /Book?name=netcore, GET /Books?name=netcore -- escape using operation name use plural words
+}
